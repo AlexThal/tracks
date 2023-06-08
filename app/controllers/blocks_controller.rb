8116c3154f3000ae054a4ccd5f20f9cc4a5df2a7
Rodrigo Cardoso <rodrigo.garrett.cardoso@gmail.com>
2023-06-08 15:47:48 +0200

    added create and delete to blocks and sets inside sessions show

---
+++ app/controllers/blocks_controller.rb
@@ -0,0 +1,30 @@
+class BlocksController < ApplicationController
+  before_action :set_session, only: %i[new create destroy]
+
+  def create
+    @block = Block.new(block_params)
+    @block.session = @session
+
+    if @block.save
+      redirect_to exercise_session_path(@session)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @block = Block.find(params[:id])
+    @block.destroy
+    redirect_to exercise_session_path(@session), status: :see_other
+  end
+
+  private
+
+  def set_session
+    @session = Session.find(params[:session_id])
+  end
+
+  def block_params
+    params.require(:block).permit(:name, :note, :gear, :rest_between_sets, :custom_field, :session_id, :category_id)
+  end
+end
