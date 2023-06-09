68f55516e21c1c8aff4699ad0c86a64b0745aa8f
Amanda Sefu Beierl <atsefu@gmail.com>
2023-06-09 15:22:02 +0200

    fixed the flash alerts and changed banner color

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -2,6 +2,7 @@ class FriendshipsController < ApplicationController
   def create
     @friend = User.find(params[:friend_id])
     @friendship = Friendship.create({ user: current_user, friend: @friend })
-    flash[:notice] = "Followed friend"
+    redirect_to root_path
+    flash[:notice] = "You are now following #{@friend.username}"
   end
 end
