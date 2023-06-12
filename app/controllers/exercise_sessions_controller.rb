d291e276bda7ee35209f7e2c8524a4c81365a32f
Amanda Sefu Beierl <atsefu@gmail.com>
2023-06-12 17:14:10 +0200

    saveas message works, needs to be added to the show page

--- app/controllers/exercise_sessions_controller.rb
+++ app/controllers/exercise_sessions_controller.rb
@@ -1,5 +1,5 @@
 class ExerciseSessionsController < ApplicationController
-  before_action :set_session, only: %i[show destroy copy_session]
+  before_action :set_session, only: %i[show destroy]
 
   def index
     @sessions = current_user.sessions
@@ -14,7 +14,6 @@ class ExerciseSessionsController < ApplicationController
   def create
     @session = Session.new(session_params)
     @session.user = current_user
-
     if params[:session][:sport_id].nil? || params[:session][:sport_id].empty?
       @sport = Sport.create(name: params[:sport_input])
     else
@@ -23,6 +22,7 @@ class ExerciseSessionsController < ApplicationController
     @session.sport = @sport
 
     if @session.save
+      load_blocks
       redirect_to exercise_session_path(@session)
     else
       render "exercise_sessions/show", status: :unprocessable_entity
@@ -40,17 +40,38 @@ class ExerciseSessionsController < ApplicationController
     redirect_to root_path, status: :see_other
   end
 
-  def copy_session
-    @copy_session = @session.dup
+  def duplicate
+    @session = Session.find(params[:session_id])
+    @duplicate_session = @session.dup
   end
 
   private
+
+  def load_blocks
+    if params[:session][:session_id]
+      old_session = Session.find(params[:session][:session_id])
+      old_session.blocks.each do |block|
+        new_block = block.dup
+        new_block.session = @session
+        new_block.save
+        load_sets(new_block)
+      end
+    end
+  end
+
+  def load_sets(block)
+    block.exercise_sets.each do |set|
+      new_set = set.dup
+      new_set.block = block
+      new_set.save
+    end
+  end
 
   def set_session
     @session = Session.find(params[:id])
   end
 
   def session_params
-    params.require(:session).permit(:title, :date, :sport)
+    params.require(:session).permit(:title, :date, :sport, :comment)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,11 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  
+
   post 'friendships/:friend_id', to: 'friendships#create', as: 'friendships'
 
   resources :sessions, only: %i[index create new], controller: 'exercise_sessions' do
+    get 'sessions/duplicate', to: 'exercise_sessions#duplicate'
     resources :blocks, only: %i[create edit destroy]
   end
   get 'sessions/:id', to: 'exercise_sessions#show', as: 'exercise_session'
